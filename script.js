373377f37de37aad7e0c99f8fc44b7f7d97c3097
CVM <s778193047@gmail.com>
2023-09-23 19:40:56 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("keypress",function(event){
+    var ran= event.key;
+
+    // for(var i=0; i<10; i++){
+    console.log(ran);
+  switch(ran){
+    // case i:{
+    //     display.value= i;
+    //     break;
+    // }
+
+    case "1":{
+        display.value+= 1;
+    }
+  }
+
+    // }
+
+
+})
